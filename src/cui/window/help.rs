32a6d5ce6457cc0780e03572bdfb5f6e369b16c9
godzie44 <godzie@yandex.ru>
2022-12-09 01:59:39 +0300

    cui: add app state

--- src/cui/window/help.rs
+++ src/cui/window/help.rs
@@ -1,9 +1,9 @@
 use crate::cui::window::{Action, CuiComponent};
-use crate::cui::AppContext;
+use crate::cui::{AppContext, AppState};
 use crossterm::event::KeyEvent;
 use std::io::StdoutLock;
 use tui::backend::CrosstermBackend;
-use tui::layout::{Alignment, Rect};
+use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Style};
 use tui::widgets::{Block, BorderType, Paragraph};
 use tui::Frame;
@@ -11,13 +11,13 @@ use tui::Frame;
 pub(super) struct ContextHelp {}
 
 impl CuiComponent for ContextHelp {
-    fn render(
-        &self,
-        _ctx: AppContext,
-        frame: &mut Frame<CrosstermBackend<StdoutLock>>,
-        rect: Rect,
-    ) {
-        let copyright = Paragraph::new("Code walker, bug stalker")
+    fn render(&self, ctx: AppContext, frame: &mut Frame<CrosstermBackend<StdoutLock>>, rect: Rect) {
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+            .split(rect);
+
+        let help = Paragraph::new("Code walker, bug stalker")
             .style(Style::default().fg(Color::LightCyan))
             .alignment(Alignment::Center)
             .block(
@@ -28,7 +28,26 @@ impl CuiComponent for ContextHelp {
                     .border_type(BorderType::Plain),
             );
 
-        frame.render_widget(copyright, rect);
+        let state_text = match ctx.state.get() {
+            AppState::Initial => "Application start",
+            AppState::DebugeeRun => "Application run",
+            AppState::DebugeeBreak => "Application break",
+            AppState::UserInput => "Wait for input",
+        };
+
+        let app_state = Paragraph::new(state_text)
+            .style(Style::default().fg(Color::Red))
+            .alignment(Alignment::Left)
+            .block(
+                Block::default()
+                    .borders(tui::widgets::Borders::ALL)
+                    .style(Style::default().fg(Color::White))
+                    .title("State")
+                    .border_type(BorderType::Rounded),
+            );
+
+        frame.render_widget(help, chunks[0]);
+        frame.render_widget(app_state, chunks[1]);
     }
 
     fn handle_user_event(&mut self, _: AppContext, _: KeyEvent) -> Vec<Action> {
