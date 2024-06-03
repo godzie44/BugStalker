8070cb64a7ac099e52ca0b589464849e3c88d821
godzie44 <godzie@yandex.ru>
2024-06-04 00:13:21 +0300

    refactoring(debugger): refactor `select` module
    - rename `VariableSelector` into `Selector`
    - rename `DqeResult` into `ScopedVariable`
    - use `Selector::by_name` instead of manual creating of `Name` selector
    - add reminder comment for oracle watchpoints

--- src/oracle/builtin/tokio.rs
+++ src/oracle/builtin/tokio.rs
@@ -1,5 +1,5 @@
 use crate::debugger::unwind::{Backtrace, FrameSpan};
-use crate::debugger::variable::select::{VariableSelector, DQE};
+use crate::debugger::variable::select::{Selector, DQE};
 use crate::debugger::variable::{ScalarVariable, StructVariable, SupportedScalar, VariableIR};
 use crate::debugger::CreateTransparentBreakpointRequest;
 use crate::debugger::{Debugger, Error};
@@ -342,11 +342,7 @@ impl TokioOracle {
     fn get_header_from_self(dbg: &mut Debugger) -> Result<Option<(u64, *const ())>, Error> {
         let header_pointer_expr = DQE::Field(
             DQE::Field(
-                DQE::Variable(VariableSelector::Name {
-                    var_name: "self".to_string(),
-                    only_local: true,
-                })
-                .boxed(),
+                DQE::Variable(Selector::by_name("self", true)).boxed(),
                 "ptr".to_string(),
             )
             .boxed(),
@@ -410,10 +406,7 @@ impl TokioOracle {
 
     fn on_new(&self, debugger: &mut Debugger) -> Result<(), Error> {
         let id_args = debugger.read_argument(DQE::Field(
-            Box::new(DQE::Variable(VariableSelector::Name {
-                var_name: "id".to_string(),
-                only_local: true,
-            })),
+            Box::new(DQE::Variable(Selector::by_name("id", true))),
             "0".to_string(),
         ))?;
 
