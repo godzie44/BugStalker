e0d3eddbfad7860f79620d35cc5550edd053fc70
TornaxO7 <tornax@proton.me>
2024-06-12 20:32:32 +0200

    adding home-manager module

---
+++ nix/home-manager-module.nix
@@ -0,0 +1,31 @@
+self: { config, lib, pkgs, ... }:
+with lib;
+let
+  bugstalker = self.packages.${pkgs.stdenv.hostPlatform.system}.bugstalker;
+  pkgs_with_bugstalker = pkgs // { inherit bugstalker; };
+
+  cfg = config.programs.bugstalker;
+  tomlFormat = pkgs.fomrats.toml { };
+in
+{
+  options.programs.bugstalker = {
+    enable = mkEnableOption "Bugstalker";
+
+    package = mkPackageOption pkgs_with_bugstalker "Bugstalker" {
+      default = [ bugstalker.pname ];
+    };
+
+    keymap = mkOption {
+      type = tomlFormat.type;
+      default = { };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    xdg.configFile."bs/keymap.toml" = lib.mkIf (cfg.keymap != { }) {
+      source = (tomlFormat.generate "keymap.toml" cfg.keymap);
+    };
+  };
+}
