4876f874d987d5f9147a5c8238bb74de578c71b1
godzie44 <godzie44@users.noreply.github.com>
2025-03-29 20:49:27 +0000

    deploy: d385b8e783e35a336bcaa4905fc291490615531d

---
+++ assets/js/4d695dc7.239b36fb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[2041],{607:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/BugStalker/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
