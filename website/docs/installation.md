c0fdff251d15923d23afe620ea81e87344fa54fc
godzie44 <godzie@yandex.ru>
2025-04-19 15:09:52 +0300

    feature(website): introduce website https://godzie44.github.io/BugStalker/

---
+++ website/docs/installation.md
@@ -0,0 +1,141 @@
+---
+sidebar_position: 2
+---
+
+# Installation
+
+## Install from sources
+
+First, check if the necessary dependencies
+(`pkg-config` and `libunwind-dev`) are installed:
+
+For example, on Ubuntu/Debian:
+
+```shell
+apt install pkg-config libunwind-dev
+```
+
+Now install the debugger:
+
+```shell
+cargo install bugstalker
+```
+
+That's all, the `bs` command is available now!
+
+<details>
+  <summary>Problem with libunwind?</summary>
+
+If you have any issues with `libunwind`, you can try to install `bs` with
+a native unwinder 
+(currently, I don't recommend this method because libunwind is better :))
+
+```shell
+cargo install bugstalker --no-default-features
+```
+
+</details>
+
+## Distro Packages
+
+<details>
+  <summary>Packaging status</summary>
+
+[![Packaging status](https://repology.org/badge/vertical-allrepos/bugstalker.svg)](https://repology.org/project/bugstalker/versions)
+
+</details>
+
+### Arch Linux
+
+```shell
+pacman -S bugstalker
+```
+
+## Nix package manager
+
+There's flake which you can use to start using it.
+Just [enable flakes](https://wiki.nixos.org/wiki/Flakes#Enable_flakes_temporarily)
+then you're able to use it with:
+
+```
+nix run github:godzie44/BugStalker
+```
+
+`BugStalker` also provides a package which you can include in your NixOS config.
+For example:
+
+<details>
+
+```nix
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    bugstalker.url = "github:godzie44/BugStalker";
+  };
+
+  outpus = {nixpkgs, bugstalker, ...}: {
+    nixosConfigurations.your_hostname = nixpkgs.lib.nixosSystem {
+      modules = [
+        ({...}: {
+          environment.systemPackages = [
+            # assuming your system runs on a x86-64 cpu
+            bugstalker.packages."x86_64-linux".default
+          ];
+        })
+      ];
+    };
+  };
+}
+```
+
+</details>
+
+### Home-Manager
+
+There's a home-manager module which adds `programs.bugstalker` to your home-manager config.
+You can add it by doing the following:
+
+<details>
+
+```nix
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    bugstalker.url = "github:godzie44/BugStalker";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, bugstalker, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.sharedModules = [
+              bugstalker.homeManagerModules.default
+              ({...}: {
+                programs.bugstalker = {
+                  enable = true;
+                  # the content of `keymap.toml`
+                  keymap = {
+                    common = {
+                      up = ["k"];
+                    }
+                  };
+                };
+              })
+            ];
+          }
+        ];
+      };
+    };
+  };
+}
+```
+
+</details>
---
+++ website/docusaurus.config.js
@@ -0,0 +1,154 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BugStalker',
+  tagline: 'Modern debugger for Linux x86-64. Written in Rust for Rust programs.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://godzie44.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/BugStalker/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'godzie44', // Usually your GitHub org/user name.
+  projectName: 'BugStalker', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+    stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/@asciinema/player/dist/themes/asciinema-player.css',
+      type: 'text/css',
+    },
+  ],
+  scripts: [
+    {
+      src: 'https://cdn.jsdelivr.net/npm/@asciinema/player/dist/asciinema-player.min.js',
+      defer: true,
+    },
+  ],
+
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/asciinema-player@3.6.3/dist/bundle/asciinema-player.min.css',
+      },
+    },
+    {
+      tagName: 'script',
+      attributes: {
+        src: 'https://cdn.jsdelivr.net/npm/asciinema-player@3.6.3/dist/bundle/asciinema-player.min.js',
+      },
+    },
+  ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'BS',
+        logo: {
+          alt: 'BS Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/godzie44/BugStalker',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Docs',
+                to: '/docs/overview',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Zulip chat',
+                href: 'https://bugstalker.zulipchat.com/join/rgqbt7cbmznbdqr47gcheomi/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/godzie44/BugStalker',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} BugStalker. All rights reserved. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+
+export default config;
