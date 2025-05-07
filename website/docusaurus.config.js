74cb09a55e5130aeb1290200abf9bfaa40ab68cf
godzie44 <godzie@yandex.ru>
2025-05-07 18:19:26 +0300

    fix(website): fix "edit this page" links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/godzie44/BugStalker/tree/master/website/',
         },
         blog: false,
         theme: {
