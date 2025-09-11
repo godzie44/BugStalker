6c30dc29a57682b6196036a244db37d353d21a73
cos <cos>
2025-09-11 12:05:15 +0200

    fix(build): fail early at compile rather than runtime

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
 fn main() {
+    if !(cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") && cfg!(target_env = "gnu")) {
+        panic!(
+            "{} only works with linux using glibc on x86_64",
+            env!("CARGO_PKG_NAME")
+        );
+    }
+
     println!("cargo:rustc-link-arg=-Wl,--export-dynamic");
     println!("cargo:rustc-link-tests=-Wl,--export-dynamic");
 }
