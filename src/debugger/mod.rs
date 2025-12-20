ed8d99816b108673fcb64f15e40d1a6fafaa8157
trgv <trgv@tavrida.com>
2025-12-21 01:09:36 +0300

    Add pause API

--- src/debugger/mod.rs
+++ src/debugger/mod.rs
@@ -757,6 +757,15 @@ impl Debugger {
         self.continue_execution()
     }
 
+    /// Interrupt (pause) execution of the whole debugee process.
+    ///
+    /// This is used by non-interactive frontends (e.g. DAP) to implement the `pause` request.
+    pub fn pause_debugee(&mut self) -> Result<(), Error> {
+        let active_bps = self.breakpoints.active_breakpoints();
+        self.debugee.pause(TraceContext::new(&active_bps, &self.watchpoints))
+    }
+
+
     /// Return list of symbols matching regular expression.
     ///
     /// # Arguments
