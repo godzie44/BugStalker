ab6c5878722ac1339db8c8275b6a9cc5dea712d1
trgv <trgv@tavrida.com>
2025-12-24 09:38:03 +0300

    Exception info

--- examples/dap_cache_vars/src/main.rs
+++ examples/dap_cache_vars/src/main.rs
@@ -1,25 +1,26 @@
 #[derive(Debug)]
 struct Inner {
-    x: i32,
-    y: i32,
+	x: i32,
+	y: i32,
 }
 
 #[derive(Debug)]
 struct Outer {
-    a: i32,
-    inner: Inner,
-    vec: Vec<Inner>,
+	a: i32,
+	inner: Inner,
+	vec: Vec<Inner>,
 }
 
 fn main() {
-    let outer = Outer {
-        a: 10,
-        inner: Inner { x: 1, y: 2 },
-        vec: vec![
-            Inner { x: 3, y: 4 },
-            Inner { x: 5, y: 6 },
-        ],
-    };
-    dbg!(&outer);
-    println!("stop");
+	let mut outer = Outer {
+		a: 10,
+		inner: Inner { x: 1, y: 2 },
+		vec: vec![Inner { x: 3, y: 4 }, Inner { x: 5, y: 6 }],
+	};
+
+	outer.a += 1; // breakpoint #1
+	outer.inner.x += 10;
+	outer.vec[0].y += 100; // breakpoint #2
+
+	println!("{outer:?}");
 }
